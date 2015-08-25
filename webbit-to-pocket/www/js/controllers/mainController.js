faf4e3b9c68ed6e893e5834ad1a5492b478442d3
Greg Harrison <gharrison@longviewinc.com>
2015-08-25 16:05:34 -0400

    Removed some ForEach loops that were used for testing.

--- webbit-to-pocket/www/js/controllers/mainController.js
+++ webbit-to-pocket/www/js/controllers/mainController.js
@@ -31,14 +31,8 @@
                         } else if (entry.length == 1) {
                             items.splice(5, Number.MAX_VALUE);
                             vm.things = items;
-                            items.forEach(function (item) {
-                                console.log(item.data.title)
-                            });
                         } else {
                             vm.things = items;
-                            items.forEach(function (item) {
-                                console.log(item.data.title)
-                            });
 
                             // "Last Updated: 3:25 pm"
                             vm.message = {description: 'Last Updated:', time: moment().format("h:mm a")};
